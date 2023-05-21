bd5f74c5d17a84dd51c48efed73874e1eee87ce9
Pedro Leal <pedroxleal10@gmail.com>
2023-05-21 00:01:43 -0300

    Create git repository

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamJuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamJuree({ subsets: ['latin'], weight: '700', variable: '--font-bai-jamjuree' });
+
+export const metadata = {
+  title: 'NLW Spacetime',
+  description: '',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
+        {children}
+      </body>
+    </html>
+  )
+}
